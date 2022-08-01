eee8dad747eb28d88335bdd7e9784e1e936b20a9
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-08-01 17:11:00 +0300

    Added BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:bloc_with_api/bloc/posts_bloc.dart';
 import 'package:bloc_with_api/view/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(BlocProvider(
+    create: (context) => PostsBloc(),
+    child:const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
         builder: (context, child) =>
-            MaterialApp(
+           const MaterialApp(
               debugShowCheckedModeBanner: false,
               home: HomeView(),
             ));
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_with_api/bloc/posts_bloc.dart';
 import 'package:bloc_with_api/constants/text_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -10,6 +12,13 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
+
+  @override
+  void initState() {
+    BlocProvider.of<PostsBloc>(context).add(GetPostsEvent());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
