231565b64651fbdf7477407c6b25a9d121d4cda4
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-08-01 12:36:57 +0300

    Added packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_with_api/view/home_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomeView(),
+    );
   }
 }
 
